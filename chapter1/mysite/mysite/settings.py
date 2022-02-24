5a14444e1cbb769e02c5834194c75b28e29705d0
ivan.s <ivan.s@icbf.io>
2022-02-24 14:35:13 +0300

    feat: admin site

--- chapter1/mysite/mysite/settings.py
+++ chapter1/mysite/mysite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.BlogConfig'
+    'chapter1.mysite.blog.apps.BlogConfig'
 ]
 
 MIDDLEWARE = [
---
+++ chapter1/setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup
+
+setup(name='django_by_example',
+      version='0.0.1',
+      packages=['chapter1.mysite'])
